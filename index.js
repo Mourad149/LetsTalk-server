8960b0b797db43237d8dd9feff9e6178eb5a696e
ZINBI Mourad <zinbimourad149@gmail.com>
2020-12-22 18:45:51 +0100

    commiting sockets updates for hand raising

--- index.js
+++ index.js
@@ -10,10 +10,25 @@ const io = require('socket.io')(http, {
 let test = 'lol';
 
 io.of('/messaging').on('connect', (socket) => {
-  socket.on('sendMessage', (data) => {
-    const { message } = data;
-    console.log(message);
-    io.of('/messaging').emit('sendToAll', { message });
+  socket.on('join', (data) => {
+    let room = data.room;
+    socket.join(room);
+    console.log(room);
+    socket.on('sendMessage', (data) => {
+      const { message, senderId } = data;
+      console.log(message);
+      io.of('/messaging').to(room).emit('sendToAll', { message, senderId });
+    });
+    socket.on('handRaised', (data) => {
+      const { senderId } = data;
+      console.log(data);
+      io.of('/messaging').to(room).emit('sendToAll', {
+        message: 'User has asked for permission to speak',
+        senderId,
+        raiseHandComponent: true,
+        userRole: data.userRole,
+      });
+    });
   });
 
   socket.on('disconnect', () => console.log('user has left'));
