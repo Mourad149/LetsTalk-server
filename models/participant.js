889f150aa362e8da629344598c3beaa6852dabeb
zakaria azziz <zakaria_azziz@um5.ac.ma>
2020-12-31 18:22:49 +0100

    Adding login and signup logic

---
+++ models/participant.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const participantSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+    currentPosition: {
+      type: String,
+      required: true,
+    },
+    mail: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+      default: "New",
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Participant", participantSchema);
